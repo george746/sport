2d8dc8736848b9a46782369d30349be15ee9b990
george746 <74596104+george746@users.noreply.github.com>
2022-05-23 11:58:52 +0200

    first commit

---
+++ forum.php
@@ -0,0 +1,13 @@
+<?php
+include_once "./php/headF.inc.php" ;
+?>
+<body>
+    <?php
+        include_once "./php/headerF.inc.php";
+        include_once "./php/connecte.inc.php" ; 
+       // include_once "./php/controle.inc.php" ;
+        include_once "./php/mainF.inc.php";
+        include_once "./php/footerf.inc.php";
+?>
+    </body> 
+</html>
